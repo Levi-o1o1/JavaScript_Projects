7cec6fc93fa3df93f9c59f0f5cbc5c9ea02f0efa
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-20 18:46:36 +0530

    maps = dict in js

---
+++ javaScript_basic/maps.js
@@ -0,0 +1,12 @@
+const myMap = new Map();
+const numberMap = new Map([[1, 'one'],[2, 'two']]);
+
+myMap.set(4, 'four')
+myMap.delete(3)
+myMap.get(1)
+myMap.has(1)
+
+console.log(myMap)
+
+
+// map is like dict in python 
