1106cc606597cc228f9dd299b5f692ba0efca4a0
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-20 19:00:49 +0530

    function 1 added

--- javaScript_basic/fucntion.js
+++ javaScript_basic/fucntion.js
@@ -2,9 +2,24 @@
 // arguments are the actual values you pass when calling the function.
 
 
-
+// type 1 of function
 function greet(name){ // name is paramenter
-    console.log("Hello" + name);
+    console.log("Hello " + name);
 }
 
 greet("ayush"); // ayush is the argument 
+
+// type 2 
+
+const problem = function(name){
+
+}
+
+// type 3
+// arrow function is new type of fucntion in es 
+const bank = (name) => {
+    return () => name
+}
+bank("rajkumar")
+bank()()
+console.log(bank)
