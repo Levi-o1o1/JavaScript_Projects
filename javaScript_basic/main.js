78129c05c980c5848172775d832488aabb86a59a
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-20 13:30:05 +0530

    if else commplte:

--- javaScript_basic/main.js
+++ javaScript_basic/main.js
@@ -1,10 +1,35 @@
 // const prompt = require("prompt-sync")()
 
-var helloWorld = "hello"
-helloWorld = "world"
+// var helloWorld = "hello"
+// helloWorld = "world"
 
 
-let x = 5
-x = 7
-x += 4
-console.log("hello world")
+// let x = 5
+// x = 7
+// x += 4
+// console.log("hello world")
+
+
+console.log("1" == 1)
+
+
+
+// # logical opreators
+// !  && ||  
+
+// console.log("" || true)
+// console.log("" && false)
+// console.log(!("" && true))
+
+
+// conditionals(if/else if / else)
+
+// if (ture){
+//     console.log('hello world')
+// }else if (false){
+//     console.log("day two")
+// }
+
+
+const cond = false ? "it is " : "no"
+console.log(cond)
