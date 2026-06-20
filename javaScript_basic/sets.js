a6f5aef5c4e66c47961658829d1dbcea7c7d39dc
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-20 18:41:48 +0530

    sets in js

---
+++ javaScript_basic/sets.js
@@ -0,0 +1,10 @@
+const nameSet = new Set();
+
+nameSet.add(1)
+nameSet.add(2)
+// nameSet.delete(1)
+nameSet.has(1)
+// nameSet.clear()
+const arr  = Array.from(nameSet)
+console.log(arr)
+console.log(nameSet)
